bc3928cbfdd00219debb406b85f107426e64ceb5
GamerRoyale <55060424+GamerRoyale@users.noreply.github.com>
2019-09-09 18:33:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,16 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
+const PREFIX = require("/config.js");
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
+    if (message.contentstartWith({PREFIX})=== 'ping') {
        message.reply(`${message.client.ping}`);
-
        }
-
 });
 
- 
 
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
