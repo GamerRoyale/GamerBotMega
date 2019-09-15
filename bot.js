f240a61144982ae5ea01a435d354029fa100011d
GamerRoyale <55060424+GamerRoyale@users.noreply.github.com>
2019-09-15 19:48:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,3 +27,17 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply(`${message.client.ping}`);
+
+       }
+
+});
