7591a9f3348e3760af0036a8caac961006cd9a5e
GamerRoyale <55060424+GamerRoyale@users.noreply.github.com>
2019-09-16 21:37:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.use === (@commands.js);
-
  
 
 client.on('ready', () => {
@@ -29,3 +27,27 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+client.on('ready', () => {
+console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+if (message.content === 'gm!founder') {
+message.reply('Gamer Royale#9637');
+}
+});
+
+
+
+client.on('ready', () => {
+console.log('I am ready!');
+});
+
+
+client.on('message', message => {
+if (message.content === 'gm!ping') {
+message.reply(`${message.client.ping}`);
+}
+});
