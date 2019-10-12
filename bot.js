d324769a0cf2d2de4faa7378a4c5b83b468c2311
GamerRoyale <55060424+GamerRoyale@users.noreply.github.com>
2019-10-12 12:03:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ console.log('I am ready!');
 
 client.on('message', message => {
 if (message.content === 'gm!ping') {
-message.reply(`${message.client.ping}`);
+message.reply`${Math.round(bot.ping)}ms`(`${message.client.ping}`);
 }
 });
 
